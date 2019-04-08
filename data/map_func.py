d4f2950e6515f3ab8184576c548e86d209aa2749
Monica Kei <yikei@microsoft.com>
2019-04-08 13:23:06 -0700

    Update notebooks for Data Prep SDK 1.1.1

--- data/map_func.py
+++ data/map_func.py
@@ -1,3 +1,4 @@
 def transform(df, index):
-    df['MAM_MTH00numvalid_1011'].fillna(0,inplace=True)
+    df['Latitude'].fillna('0', inplace=True)
+    df['Longitude'].fillna('0', inplace=True)
     return df
